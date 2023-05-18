fc3546210e35ad46a66763aa64ff2ef2a0d95080
Joyce Njoroge <joycewambuku33@gmail.com>
2023-05-18 16:36:17 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,16 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+   def __init__(self, name ="Mutt", breed=None):
+        self.breed = breed
+
+        if not isinstance(name, str) or name == "" or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name   
+
+        if breed is not None and breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+
+fido = Dog(name="Fido")
+fido = Dog(breed="Human")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,20 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Guido", job=None):
+        self.job = job
+        if not isinstance(name, str) or name == "":
+            print("Name must be string between 1 and 25 characters.")
+        
+        elif len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name.title()
+
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self.job = job    
+        
+guido = Person(name="Guido", job="Sales")           
+            
